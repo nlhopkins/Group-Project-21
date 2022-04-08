3a46cce71548dbc8c08604a41c44a27d858b7203
nlhopkins <natashahopkins1@hotmail.co.uk>
2022-04-09 00:26:14 +0100

    Update

---
+++ report/notes.R
@@ -0,0 +1,43 @@
+-   Cancer is a very different context - you should make this clear, and if it's the basis of choosing it as a target mention it in the introduction. If it's not regulated by Wnt in this context - then that is fine but you need to be quite explicit in how you discuss it.
+
+<!-- -->
+
+-   Maybe - how could you find out?
+    Also - is this canonical or non-canonical Wnt?
+    There is cross talk between the two kinds of pathway, but they are quite separate.
+    XAV939 won't inhibit non-canonical Wnt signalling.
+
+    -   These Are where the marks are. You should think about how you could confirm if you are inhibiting signalling (usually a WEstern blot for phospho proteins involved in the signalling pathway - if you look up the vendor page of the inhibition there is usually a paper where they confirm the inhibitor in some system or another).
+
+-   If Inhibitor failed, why not consistent with the DMSO samples?
+
+    -   XAV939 works but issue with primers specificity? Bind to another protein upregulated in LSS. Wnt downregulated in HSS hence why fold changes smaller? Small sample size so downregulation seems more prominent then it actually is
+
+<!-- -->
+
+-   Possible Errors and Refine Methods
+
+    -   Small sample = not enough to confirm spectrophotometer was consistent
+
+    -   No prior PCR experience
+
+-   Melt curve suggests primer dimers and contamination (include images of curves or include in supplementary?)
+
+-   en face staining for gene markers in inner curvature [@Warboys2014]
+
+-   Direct interaction with Wnt or via VEGF?
+
+    -   *THSB1* inhibits VEGF by competing for heparin (which is used in our medium) [@gupta1999; @dias2012; @tolsma1993]
+
+-   Then you can start tying in non canonical Wnt signalling and seeing whether there is cross talk with either this pathway or other pathways that could explain the result.
+    Then what sort of experiments in other systems you could do.
+
+-   Methods used in other studies
+
+-   animal studies, zebrafish, mice, pigs [@serbanovic-canic2017; @Li2014-mx; @farhat2013; @Moura2008; @moonen2015
+
+-   non-canonical wnt [@franco2016]
+
+-   relative vs quantitative expression
+
+-   flow chamber instead of orbital shaker
